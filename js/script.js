ce3c9570888fdfc1ef8a5df9b2a39db683170b9e
sasocr9 <salah3hossam@gmail.com>
2024-06-25 20:38:18 +0300

    v6

--- js/script.js
+++ js/script.js
@@ -1,21 +1,22 @@
-// import AOS from "aos";
-// // import "aos/dist/aos.css";
+// Import AOS library and styles
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-// // init AOS animation
-// // AOS.init({
-// //   duration: 1000,
-// //   offset: 100,
-// // });
+// Initialize AOS animations
+AOS.init({
+  duration: 1000,
+  offset: 100,
+});
 
-// the additional code you saw in the video will not be needed :)
-
+// Select all elements with the class 'popular-foods__card'
 let cards = document.querySelectorAll(".popular-foods__card");
 
-cards.forEach(function (i) {
-  i.addEventListener("mouseenter", function () {
-    i.classList.add("active-card");
+// Add event listeners to each card for mouse enter and leave events
+cards.forEach(function (card) {
+  card.addEventListener("mouseenter", function () {
+    card.classList.add("active-card");
   });
-  i.addEventListener("mouseleave", function () {
-    i.classList.remove("active-card");
+  card.addEventListener("mouseleave", function () {
+    card.classList.remove("active-card");
   });
 });
