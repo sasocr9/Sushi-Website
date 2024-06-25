99dc305df73afec8a3c7f7ef01ebae52958d6661
sasocr9 <salah3hossam@gmail.com>
2024-06-25 20:35:22 +0300

    v5

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos";
+// // import "aos/dist/aos.css";
 
-// init AOS animation
-AOS.init({
-    duration: 1000,
-    offset: 100,
-});
+// // init AOS animation
+// // AOS.init({
+// //   duration: 1000,
+// //   offset: 100,
+// // });
 
 // the additional code you saw in the video will not be needed :)
+
+let cards = document.querySelectorAll(".popular-foods__card");
+
+cards.forEach(function (i) {
+  i.addEventListener("mouseenter", function () {
+    i.classList.add("active-card");
+  });
+  i.addEventListener("mouseleave", function () {
+    i.classList.remove("active-card");
+  });
+});
